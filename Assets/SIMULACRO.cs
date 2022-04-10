8913001b8cc26c8d04060ea60a0a8210b38a5a65
iangarber26 <88200710+iangarber26@users.noreply.github.com>
2022-04-10 18:50:28 -0300

    Exercise 1 completed

---
+++ Assets/SIMULACRO.cs
@@ -0,0 +1,31 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SIMULACRO : MonoBehaviour
+{
+
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (num1 == num2)
+        {
+            Debug.Log("Son iguales");
+        }
+        else
+        {
+            Debug.Log("No son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
